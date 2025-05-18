715742c962966d9f7912faa400179ade4d2d7b37
Deepak Sharma <deepaucksharma@gmail.com>
2025-05-18 17:44:09 +0530

    chore: add eslint config and husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
